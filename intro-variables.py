0e27286dfcf8a55ac017441b81652c469aead58f
0xTheSoul <0xTheSoul@proton.me>
2026-02-25 23:04:24 +0530

    update

---
+++ intro-variables.py
@@ -0,0 +1,34 @@
+#Variable = A container for a value.
+
+#Strings
+name = "Soul"
+language = "python"
+
+print(f"My name is {name}")
+print(f"This is a {language} program")
+
+#Integer
+
+num_of_oceans = 7
+num_of_bitcoins = 21
+
+print(f"There are {num_of_oceans} oceans in the whole world.")
+print("by the way")
+print(f"I don't have {num_of_bitcoins} bitcoins, it's a bluff.")
+
+#Float
+
+price = 9.99
+
+print(f"Wanna buy a Bitcoin for ${price}?")
+
+#Boolean
+
+Money = False
+
+if Money: 
+    print("You can buy Bitcoin")
+else:
+    print("You are broke")
+
+    #Alright! All done for his one.
