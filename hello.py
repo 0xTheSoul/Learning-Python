99342e10f1f1fb8053e2f830b81ea8ced53525b3
0xTheSoul <0xTheSoul@proton.me>
2026-02-25 21:51:07 +0530

    update

--- hello.py
+++ hello.py
@@ -1,3 +1,5 @@
-print("Hello World")
-user = "Human"
-print(f"hello {user}")
+#Hello world! This is my first Python program,thanks @BroCodez.
+print("Hello everyone!")
+print("I am Soul")
+print("I am learning how to code")
+print("Python is my first programing language")
