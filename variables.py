0a92bcd423591b5ac7568b1bccc7aea9e6c866dd
0xTheSoul <0xTheSoul@proton.me>
2026-02-26 22:18:29 +0530

    new

---
+++ variables.py
@@ -0,0 +1,14 @@
+#practice_variables
+
+name = "soul"
+age = 2026
+height = 0.7
+drunk = True
+
+print(f"name: {name}")
+print(f"age: {age}years")
+print(f"height: {height}nm")
+if drunk:
+    print("i am not human")
+else:
+    print("i am god")
